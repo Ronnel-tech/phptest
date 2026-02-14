e6584d75e9a3e16802df66fcb13e8a0e700de7be
Ronnel <logdatronnell@gmail.com>
2026-02-15 04:08:38 +0800

    pagod na ngani

--- index.php
+++ index.php
@@ -6,11 +6,36 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
 
-    echo ("i dont like the color red");
-    echo ("i like the color blue");
+<form action= "index.php" method="post">
+    <input type="checkbox" name = "pizza" value = "Pizza">Pizza<br>
+    <input type="checkbox" name = "burger" value = "Burger">Burger<br>
+    <input type="checkbox" name = "fries" value = "Fries">Fries<br>
+    <input type="checkbox" name = "soda" value = "Soda">Soda<br>
+    <input type="submit" name="submit" value="Submit">
     
-    ?>
+</form>
 </body>
 </html>
+
+    <?php 
+    if(isset($_POST["submit"]))
+        {
+            if(isset($_POST["pizza"]))
+                {
+                    echo "You like pizza!<br>";
+                }
+            elseif(isset($_POST["burger"]))
+                {
+                    echo "You like burger!<br>";
+                }
+            elseif(isset($_POST["fries"]))
+                {
+                    echo "You like fries!<br>";
+                }
+            elseif(isset($_POST["soda"]))
+                {
+                    echo "You like soda!<br>";
+                } 
+        }
+    ?>
