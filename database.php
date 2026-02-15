3e141660b61a58554e2b5dcd36a1ff8e07bb7358
Ronnel <logdatronnell@gmail.com>
2026-02-15 23:22:43 +0800

    patapos na bro

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php 
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "businessdb";
+$conn = "";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    if ($conn) {
+        echo "Connection successful!";
+    } else {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +9,56 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="index.php" method="post">
+        <!-- <input type="text" name="username" placeholder="Enter your username">  -->
+        <br>
+        <br>
+        <input type="password" name= "password" placeholder="Enter your password">
+        <br>
+        <input type="submit" value="Submit" name="Submit">
+    </form>
 </body>
 </html>
 
     <?php
-    include "header.html";
+    // if (isset($_POST["Submit"]))
+    // {
+
+    //     if(!empty($_POST['username']) && !empty($_POST['password']))
+    //     {
+    //         $_SESSION['username'] = $_POST['username'];
+    //         $_SESSION['password'] = $_POST['password'];
+
+    //         header("Location: home.php");
+    //     }
+        
+    // }
+    // foreach($_SERVER as $key => $value)
+    //     {
+    //         echo "$key => $value <br>";
+    //     }
+
+    $password = "12345";
+    $ngek = password_hash($password, PASSWORD_DEFAULT);
+    // echo $ngek;
+
+    if(isset($_POST["Submit"]))
+        {
+            if(!empty($_POST["password"]))
+                {
+                    if(password_verify($_POST["password"], $ngek))
+                    {
+                        $_SESSION["password"] = $_POST["password"];
+                        header("Location: home.php");
+                    }
+                    else
+                    {
+                        echo "Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    echo "Please enter your password!";
+                }
+        }
     ?>
