62138ae228005a9c9a7620057ca50cae4a146b3b
Ronnel <logdatronnell@gmail.com>
2026-02-16 01:32:36 +0800

    pass na

--- database.php
+++ database.php
@@ -8,9 +8,9 @@ $conn = "";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-    if ($conn) {
-        echo "Connection successful!";
-    } else {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    // if ($conn) {
+    //     echo "Connection successful!";
+    // } else {
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
 ?>
--- index.php
+++ index.php
@@ -1,22 +1,36 @@
 <?php
 session_start();
+include "database.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Document</title>
 </head>
-<body>
-    <form action="index.php" method="post">
+<body class="bg-[#151f2e] ml-10 mr-lg mt-lg">
+    <!-- <form action="index.php" method="post"> -->
         <!-- <input type="text" name="username" placeholder="Enter your username">  -->
-        <br>
+        <!-- <br>
         <br>
         <input type="password" name= "password" placeholder="Enter your password">
         <br>
         <input type="submit" value="Submit" name="Submit">
-    </form>
+    </form> -->
+
+
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?> method = "post">
+
+    <div class="bg-[#151f2e] w-1/2 mx-auto mt-100 justify-center items-center p-5 rounded-lg flex flex-col items-center gap-5 outline outline-1 outline-white">
+        <input type="text" name = "username" placeholder = "Enter your username" class= " bg-gray-300 px-4 py-2 rounded-lg mt-5 pl-xl">
+        <input type="password" name = "password" placeholder = "Enter your password" class= " bg-gray-300 px-4 py-2 rounded-lg mt-5 pl-xl">
+        <input type="submit" value="Submit" name="Submit" class="bg-gray-300 px-4 py-2 rounded-lg mt-5">
+
+
+    </div>
+</form>
 </body>
 </html>
 
@@ -38,27 +52,43 @@ session_start();
     //         echo "$key => $value <br>";
     //     }
 
-    $password = "12345";
-    $ngek = password_hash($password, PASSWORD_DEFAULT);
-    // echo $ngek;
+    // $password = "12345";
+    // $ngek = password_hash($password, PASSWORD_DEFAULT);
+    // // echo $ngek;
 
-    if(isset($_POST["Submit"]))
-        {
-            if(!empty($_POST["password"]))
-                {
-                    if(password_verify($_POST["password"], $ngek))
-                    {
-                        $_SESSION["password"] = $_POST["password"];
-                        header("Location: home.php");
-                    }
-                    else
-                    {
-                        echo "Password is incorrect!";
-                    }
-                }
-                else
-                {
-                    echo "Please enter your password!";
-                }
-        }
+    // if(isset($_POST["Submit"]))
+    //     {
+    //         if(!empty($_POST["password"]))
+    //             {
+    //                 if(password_verify($_POST["password"], $ngek))
+    //                 {
+    //                     $_SESSION["password"] = $_POST["password"];
+    //                     header("Location: home.php");
+    //                 }
+    //                 else
+    //                 {
+    //                     echo "Password is incorrect!";
+    //                 }
+    //             }
+    //             else
+    //             {
+    //                 echo "Please enter your password!";
+    //             }
+    //     }
+
+// $sql = "INSERT INTO users (username, password) VALUES ('deds', 'admin123')";
+// mysqli_query($conn, $sql);
+
+// $sql = "SELECT * FROM users WHERE username = 'deds' ";
+// $result = mysqli_query($conn, $sql);
+
+// if(mysqli_num_rows($result) > 0)
+//     {
+//         $row = mysqli_fetch_assoc($result);
+//         echo "Username: " . $row["username"] . "<br>";
+//         echo "Password: " . $row["password"] . "<br>";
+//         echo "Registration Date: {$row['reg_date']} <br>";
+//     }
+
+// mysqli_close($conn);
     ?>
